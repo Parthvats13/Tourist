bae2880bebb1b52b56d413736922bf8b87202ce4
Parthvats13 <vatsparth22@gmail.com>
2025-04-11 21:53:34 +0530

    Created the basic app Interface

--- Tourist/TouristApp.swift
+++ Tourist/TouristApp.swift
@@ -8,10 +8,17 @@
 import SwiftUI
 
 @main
-struct TouristApp: App {
+struct HimYatraApp: App {
+    @StateObject private var userProfile = UserProfileViewModel()
+    @StateObject private var tripPlanner = TripPlannerViewModel()
+    @StateObject private var locationManager = LocationManager()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(userProfile)
+                .environmentObject(tripPlanner)
+                .environmentObject(locationManager)
         }
     }
 }
