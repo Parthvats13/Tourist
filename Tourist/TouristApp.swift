3bbf0500b88e8e88cc33a51d2d344efaab3ac36d
Parthvats13 <vatsparth22@gmail.com>
2025-04-11 14:33:00 +0530

    Initial Commit

---
+++ Tourist/TouristApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouristApp.swift
+//  Tourist
+//
+//  Created by Parth Vats on 11/04/25.
+//
+
+import SwiftUI
+
+@main
+struct TouristApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
